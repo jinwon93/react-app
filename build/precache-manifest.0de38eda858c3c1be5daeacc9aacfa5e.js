98cbbe73796d60da8b3e23419e33da369b82e696
username <kjw8208@nate.com>
2020-07-02 12:30:56 +0900

    app1

---
+++ build/precache-manifest.0de38eda858c3c1be5daeacc9aacfa5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7effeed641f223b786257a3eb8ded182",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d54b0a3d58736a196401",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "2b4c1199f1e002dcdb8b",
+    "url": "/static/js/2.e1eeb85d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e1eeb85d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d54b0a3d58736a196401",
+    "url": "/static/js/main.e21735f3.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  }
+]);
